4ce63f9b5274272560b5ec2546abea507541c5c8
BuildTools <unconfigured@null.spigotmc.org>
2018-04-02 13:33:53 +0200

    import--

--- ExplosiveEggs/src/main/java/nl/rmoesbergen/mcplugin/McPlugin.java
+++ ExplosiveEggs/src/main/java/nl/rmoesbergen/mcplugin/McPlugin.java
@@ -3,7 +3,6 @@ package nl.rmoesbergen.mcplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEggThrowEvent;
