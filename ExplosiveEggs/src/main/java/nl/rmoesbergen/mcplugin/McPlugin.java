b4422290e2df23aac736293738645c631e443d99
BuildTools <unconfigured@null.spigotmc.org>
2018-04-02 12:35:43 +0200

    Explosive Eggs plugin (Bukkit)

---
+++ ExplosiveEggs/src/main/java/nl/rmoesbergen/mcplugin/McPlugin.java
@@ -0,0 +1,37 @@
+package nl.rmoesbergen.mcplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+
+import java.util.logging.Logger;
+
+public final class McPlugin extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		Logger logger = getLogger();
+		logger.info("McPlugin Enabled");
+		
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable() {
+		Logger logger = getLogger();
+		logger.info("McPlugin Disabled");
+	}
+	
+	@EventHandler
+	public static void onEggThrown(PlayerEggThrowEvent event) {
+		Player player = event.getPlayer();
+
+		Location loc = player.getTargetBlock(null, 0).getLocation();
+		TNTPrimed tnt = player.getWorld().spawn(loc, TNTPrimed.class);
+		tnt.setFuseTicks(0);
+	}
+}
