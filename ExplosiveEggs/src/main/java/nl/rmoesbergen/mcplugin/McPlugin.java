916774a121bf9d6410c74a714eaa1a2ebea05c1a
Ronald Moesbergen <ronald.moesbergen@solvinity.com>
2018-04-21 13:33:19 +0200

    Add toggle command

--- ExplosiveEggs/src/main/java/nl/rmoesbergen/mcplugin/McPlugin.java
+++ ExplosiveEggs/src/main/java/nl/rmoesbergen/mcplugin/McPlugin.java
@@ -4,7 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -22,6 +23,7 @@ import java.util.logging.Logger;
 public final class McPlugin extends JavaPlugin implements Listener {
 
 	private Location lastPosition;
+	private boolean devilEnabled = false;
 
 	@Override
 	public void onEnable() {
@@ -37,6 +39,16 @@ public final class McPlugin extends JavaPlugin implements Listener {
 		logger.info("McPlugin Disabled");
 	}
 
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("devil")) {
+			devilEnabled = !devilEnabled;
+			getLogger().info("Devil mode " + devilEnabled);
+			return true;
+		}
+		return false;
+	}
+
 	/* Disable player damage */
 	@EventHandler
 	public void onEntityDamage(EntityDamageEvent event) {
@@ -49,6 +61,8 @@ public final class McPlugin extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
+		if (!devilEnabled) return;
+		
 		Player player = event.getPlayer();
 		Location loc = player.getLocation();
 
@@ -71,8 +85,8 @@ public final class McPlugin extends JavaPlugin implements Listener {
 
 		if (event.getEntityType() == EntityType.EGG) {
 			Location loc = event.getEntity().getLocation();
-			int radius = 5;
 
+/*			int radius = 5;
 			int y = loc.getBlockY() + 2;
 			for (int x = loc.getBlockX() - radius; x <= loc.getBlockX() + radius; x++)
 				for (int z = loc.getBlockZ() - radius; z <= loc.getBlockZ() + radius; z++) {
@@ -84,16 +98,18 @@ public final class McPlugin extends JavaPlugin implements Listener {
 						// Location curloc = new Location(world, x, y , z);
 						// world.spawnFallingBlock(curloc, Material.FENCE, (byte)0);
 					}
-				}
+				}*/
 
-			/*
-			 * Sphere sphere = new Sphere(); world.createExplosion(loc, 4F);
-			 * sphere.Draw(loc, 5F, Material.GLASS); loc.add(0, 5, 0); for (int count = 0;
-			 * count < 10; count++) { Firework firework = (Firework)world.spawnEntity(loc,
-			 * EntityType.FIREWORK); //firework.setMetadata(arg0, arg1); // Fireball
-			 * fireball = (Fireball)world.spawnEntity(loc, EntityType.FIREBALL); //
-			 * fireball.setDirection(new Vector(loc.getX(), loc.getY()+10F, loc.getZ())); }
-			 */ }
+			Sphere sphere = new Sphere();
+			world.createExplosion(loc, 4F);
+			sphere.Draw(loc, 5F, Material.GLASS);
+			loc.add(0, 5, 0);
+			for (int count = 0; count < 10; count++) {
+				world.spawnEntity(loc, EntityType.FIREWORK);
+				//fireball = (Fireball) world.spawnEntity(loc, EntityType.FIREBALL);
+				//fireball.setDirection(new Vector(loc.getX(), loc.getY() + 10F, loc.getZ()));
+			}
+		}
 	}
 }
 
