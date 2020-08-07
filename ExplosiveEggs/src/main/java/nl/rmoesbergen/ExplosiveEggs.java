c1fbaecaeaa63e29f43ffed91fd140fce0f83457
Ronald Moesbergen <github@rmoesbergen.nl>
2020-08-07 16:45:27 +0200

    add game element (hitting other players, bomb size increase)

--- ExplosiveEggs/src/main/java/nl/rmoesbergen/ExplosiveEggs.java
+++ ExplosiveEggs/src/main/java/nl/rmoesbergen/ExplosiveEggs.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import com.sk89q.worldedit.world.block.BlockTypes;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -19,6 +20,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 public final class ExplosiveEggs extends JavaPlugin implements Listener {
+
+    private static final String METADATA_BOMB_RADIUS = "egg-bomb-radius";
+    private static final String METADATA_JUMPING = "jumping";
 
     @Override
     public void onEnable() {
@@ -131,28 +135,57 @@ public final class ExplosiveEggs extends JavaPlugin implements Listener {
 	}
 	*/
 
+    private void increaseEggBombSize(Player player) {
+        if (player.hasMetadata(METADATA_BOMB_RADIUS)) {
+            double currentValue = player.getMetadata(METADATA_BOMB_RADIUS).get(0).asDouble();
+            player.setMetadata(METADATA_BOMB_RADIUS, new FixedMetadataValue(this, currentValue + 1F));
+        } else {
+            player.setMetadata(METADATA_BOMB_RADIUS, new FixedMetadataValue(this, 6F));
+        }
+    }
+
+    private double getEggBombSize(Player player) {
+        if (player.hasMetadata(METADATA_BOMB_RADIUS)) {
+            return player.getMetadata(METADATA_BOMB_RADIUS).get(0).asDouble();
+        } else {
+            return 5F;
+        }
+    }
+
     @EventHandler
     public void onEggHits(ProjectileHitEvent event) {
 
         World world = event.getEntity().getWorld();
+        if (!(event.getEntityType() == EntityType.EGG)) return;
 
-        if (event.getEntityType() == EntityType.EGG) {
+        Projectile egg = event.getEntity();
+        if (!(egg.getShooter() instanceof Player)) return;
 
-            Projectile egg = event.getEntity();
-            if (!(egg.getShooter() instanceof Player)) return;
+        Player player = (Player) egg.getShooter();
+        if (!player.hasPermission("explosiveeggs.throw")) return;
 
-            Player player = (Player) egg.getShooter();
-            if (!player.hasPermission("explosiveeggs.throw")) return;
+        Location eggLocation = egg.getLocation();
 
-            Location loc = event.getEntity().getLocation();
+        // Draw a glass sphere + explosion where the egg hits
+        Sphere sphere = new Sphere();
+        double radius = getEggBombSize(player);
+        world.createExplosion(eggLocation, 4F);
+        sphere.Draw(eggLocation, radius, BlockTypes.GLASS);
+        eggLocation.add(0, radius, 0);
+        world.spawnEntity(eggLocation, EntityType.FIREWORK);
 
-            Sphere sphere = new Sphere();
-            world.createExplosion(loc, 4F);
-            sphere.Draw(loc, 5F, BlockTypes.GLASS);
-            loc.add(0, 5, 0);
-            for (int count = 0; count < 3; count++) {
-                world.spawnEntity(loc, EntityType.FIREWORK);
-            }
+        // If hitting another player
+        // TODO: improve this to check if hit player is inside player's glass bubble
+        Entity hitEntity = event.getHitEntity();
+        if (hitEntity instanceof Player) {
+            // Increase player's bomb size
+            increaseEggBombSize(player);
+
+            // Reset hit player's bomb size
+            Player hitPlayer = (Player) hitEntity;
+            hitPlayer.removeMetadata(METADATA_BOMB_RADIUS, this);
+
+            player.sendTitle("You hit " + hitPlayer.getName() + "!", "Your bomb size has increased",10,70,20);
         }
     }
 
@@ -161,11 +194,11 @@ public final class ExplosiveEggs extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         if (!player.hasPermission("explosiveeggs.jump")) return;
 
-        if (event.getFrom().getY() < event.getTo().getY() && !player.hasMetadata("jumping")) {
+        if (event.getFrom().getY() < event.getTo().getY() && !player.hasMetadata(METADATA_JUMPING)) {
             player.setVelocity(event.getPlayer().getVelocity().add(new Vector(0, 1, 0)));
-            player.setMetadata("jumping", new FixedMetadataValue(this, true));
+            player.setMetadata(METADATA_JUMPING, new FixedMetadataValue(this, true));
         } else if (player.isOnGround()) {
-            player.removeMetadata("jumping", this);
+            player.removeMetadata(METADATA_JUMPING, this);
         }
     }
 }
